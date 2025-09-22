d3e0bf6e064cbbe12025725158c7fd47715fb7e5
yashkoladiya1 <koladiyayash104@gmail.com>
2025-09-22 11:02:14 +0200

    Strings modify and concatenation

---
+++ 7.Strings_Modify.py
@@ -0,0 +1,26 @@
+# Upper Case
+a = "Hello, World!"
+print("Upper Case----->",a.upper())
+
+#Lower Case
+b = "Hello, World!"
+print("Lower Case---->",b.lower())
+
+#Remove Whitespace
+c = " Hello World "
+print("remove space--->",c.strip())
+
+#Replace String
+d = "Hello, World!"
+print("Replace String--->",d.replace("H","J"))
+
+#Split String
+e = "Hello, World!"
+print("split string---->",e.split(","))
+
+#String Concatenation
+
+r = "Hello"
+t = "World"
+u = r + t
+print("Concatenation---->",u)
