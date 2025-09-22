8a894148883f17e406c60b024e2455b822b89f75
yashkoladiya1 <koladiyayash104@gmail.com>
2025-09-22 14:28:08 +0200

    Operators

---
+++ 10.Operators.py
@@ -0,0 +1,26 @@
+"""
+Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators
+"""
+
+print("Addition--->",5+2)
+print("Subtraction--->",5-2)
+print("Multiplication--->",5*2)
+print("Division--->",5/2)
+print("Modulus--->",5%2)
+print("Exponentiation--->",5**2)
+print("Floor division--->",5//2)
+
+#Python Comparison Operators
+
+print("Equal---->",5==5)
+print("Not equal---->",5!=5)
+print("Greater than---->",5>3)
+print("Less than---->",5<2)
+print("Greater than or equal to---->",5>=5)
+print("Less than or equal to---->",5<=5)
