51fb215198ceb667eaf1cebc795716c098fe22d8
yashkoladiya1 <koladiyayash104@gmail.com>
2025-09-22 11:24:23 +0200

    Boolean

---
+++ 9.Boolean.py
@@ -0,0 +1,13 @@
+#Boolean Values
+"""
+In programming you often need to know if an expression is True or False.
+
+You can evaluate any expression in Python, and get one of two answers, True or False.
+
+When you compare two values, the expression is evaluated and Python returns the Boolean answer:
+
+"""
+
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
